0df5a728e17bb0cb52948ba57387cb766526f428
navigatrum <ab@Lenovo-PC.localdomain>
2020-06-17 18:15:28 +0200

    sh install_wp postponed download

--- templates/install-wp-tests.sh
+++ templates/install-wp-tests.sh
@@ -62,7 +62,7 @@ install_wp() {
 
     mkdir -p $WP_CORE_DIR
 
-    if [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
+    if [ "$WP_VERSION" = 'nightly' ] || [ "$WP_VERSION" = 'trunk' ]; then
         mkdir -p $TMPDIR/wordpress-nightly
         download https://wordpress.org/nightly-builds/wordpress-latest.zip  $TMPDIR/wordpress-nightly/wordpress-nightly.zip
         unzip -q $TMPDIR/wordpress-nightly/wordpress-nightly.zip -d $TMPDIR/wordpress-nightly/
@@ -70,18 +70,18 @@ install_wp() {
     else
         if [ $WP_VERSION == 'latest' ]; then
             local ARCHIVE_NAME='latest'
-        elif [[ $WP_VERSION =~ [0-9]+\.[0-9]+ ]]; then
-            # https serves multiple offers, whereas http serves single.
-            download https://api.wordpress.org/core/version-check/1.7/ $TMPDIR/wp-latest.json
-            if [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0] ]]; then
+        elif echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+'; then
+            if echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0]'; then
                 # version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
                 LATEST_VERSION=${WP_VERSION%??}
             else
+                # https serves multiple offers, whereas http serves single.
+                download https://api.wordpress.org/core/version-check/1.7/ $TMPDIR/wp-latest.json
                 # otherwise, scan the releases and get the most up to date minor version of the major release
                 local VERSION_ESCAPED=`echo $WP_VERSION | sed 's/\./\\\\./g'`
                 LATEST_VERSION=$(grep -o '"version":"'$VERSION_ESCAPED'[^"]*' $TMPDIR/wp-latest.json | sed 's/"version":"//' | head -1)
             fi
-            if [[ -z "$LATEST_VERSION" ]]; then
+           if [ -z "$LATEST_VERSION" ]; then
                 local ARCHIVE_NAME="wordpress-$WP_VERSION"
             else
                 local ARCHIVE_NAME="wordpress-$LATEST_VERSION"
