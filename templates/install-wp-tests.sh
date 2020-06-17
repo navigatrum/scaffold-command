942f5a1c826837fd403f8d35729b5247b2ab857d
navigatrum <ab@Lenovo-PC.localdomain>
2020-06-17 18:07:24 +0200

    sh tests_tag external

--- templates/install-wp-tests.sh
+++ templates/install-wp-tests.sh
@@ -15,6 +15,34 @@ TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
 WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
+if echo "$WP_VERSION" | grep -Eq '^[0-9]+\.[0-9]+\-(beta|RC)[0-9]+$'; then
+    WP_BRANCH=${WP_VERSION%\-*}
+    WP_TESTS_TAG="branches/$WP_BRANCH"
+elif echo "$WP_VERSION" | grep -Eq '^[0-9]+\.[0-9]+$'; then
+    WP_TESTS_TAG="branches/$WP_VERSION"
+elif echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0-9]+'; then
+    if echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0]'; then
+        # version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
+        WP_TESTS_TAG="tags/${WP_VERSION%??}"
+    else
+        WP_TESTS_TAG="tags/$WP_VERSION"
+    fi
+elif [ "$WP_VERSION" = 'nightly' ] || [ "$WP_VERSION" = 'trunk' ]; then
+    WP_TESTS_TAG="trunk"
+elif [ "$WP_VERSION" = 'latest' ]; then
+    wget -nv -O $TMPDIR/wp-latest.json http://api.wordpress.org/core/version-check/1.7/
+    grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /$TMPDIR/wp-latest.json
+    LATEST_VERSION=$(grep -o '"version":"[^"]*' /$TMPDIR/wp-latest.json | sed 's/"version":"//')
+    if [ -z "$LATEST_VERSION" ]; then
+        echo "Latest WordPress version could not be found"
+        exit 1
+    fi
+    WP_TESTS_TAG="tags/$LATEST_VERSION"
+else
+    echo "Invalid Wordpress version name."
+    exit 1
+fi
+
 download() {
     if [ `which curl` ]; then
         curl -s "$1" > "$2";
@@ -69,32 +97,7 @@ install_wp() {
 }
 
 install_test_suite() {
-    if [[ $WP_VERSION =~ ^[0-9]+\.[0-9]+\-(beta|RC)[0-9]+$ ]]; then
-        WP_BRANCH=${WP_VERSION%\-*}
-        WP_TESTS_TAG="branches/$WP_BRANCH"
 
-    elif [[ $WP_VERSION =~ ^[0-9]+\.[0-9]+$ ]]; then
-        WP_TESTS_TAG="branches/$WP_VERSION"
-    elif [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
-        if [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0] ]]; then
-            # version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
-            WP_TESTS_TAG="tags/${WP_VERSION%??}"
-        else
-            WP_TESTS_TAG="tags/$WP_VERSION"
-        fi
-    elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-        WP_TESTS_TAG="trunk"
-    else
-        # http serves a single offer, whereas https serves multiple. we only want one
-        download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
-        grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
-        LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
-        if [[ -z "$LATEST_VERSION" ]]; then
-            echo "Latest WordPress version could not be found"
-            exit 1
-        fi
-        WP_TESTS_TAG="tags/$LATEST_VERSION"
-    fi
     # portable in-place argument for both GNU sed and Mac OSX sed
     if [[ $(uname -s) == 'Darwin' ]]; then
         local ioption='-i.bak'
