9cf15acbcade8a7d4e344696f2c7b11f28f64659
navigatrum <ab@Lenovo-PC.localdomain>
2020-06-19 01:01:07 +0200

    normalize code

--- templates/install-wp-tests.sh
+++ templates/install-wp-tests.sh
@@ -18,33 +18,33 @@ WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
 download() {
-    if [ `which curl` ]; then
-        curl -s "$1" > "$2";
-    elif [ `which wget` ]; then
-        wget -nv -O "$2" "$1"
-    fi
+	if [ `which curl` ]; then
+		curl -s "$1" > "$2";
+	elif [ `which wget` ]; then
+		wget -nv -O "$2" "$1"
+	fi
 }
 
-if echo "$WP_VERSION" | grep -Eq '^[0-9]+\.[0-9]+\-(beta|RC)[0-9]+$'; then
-    WP_BRANCH=${WP_VERSION%\-*}
-    WP_TESTS_TAG="branches/$WP_BRANCH"
-elif echo "$WP_VERSION" | grep -Eq '^[0-9]+\.[0-9]+$'; then
-    WP_TESTS_TAG="branches/$WP_VERSION"
-elif echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0-9]+'; then
-    if echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0]'; then
-        # version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
-        WP_TESTS_TAG="tags/${WP_VERSION%??}"
-    else
-        WP_TESTS_TAG="tags/$WP_VERSION"
-    fi
+if [ $(echo $WP_VERSION | grep -Eq '^[0-9]+\.[0-9]+\-(beta|RC)[0-9]+$') ]; then
+	WP_BRANCH=${WP_VERSION%\-*}
+	WP_TESTS_TAG="branches/$WP_BRANCH"
+elif [ $(echo $WP_VERSION | grep -Eq '^[0-9]+\.[0-9]+$') ]; then
+	WP_TESTS_TAG="branches/$WP_VERSION"
+elif [ $(echo $WP_VERSION | grep -Eq '[0-9]+\.[0-9]+\.[0-9]+') ]; then
+	if [ $(echo $WP_VERSION | grep -Eq '[0-9]+\.[0-9]+\.[0]') ]; then
+		# version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
+		WP_TESTS_TAG="tags/${WP_VERSION%??}"
+	else
+		WP_TESTS_TAG="tags/$WP_VERSION"
+	fi
 elif [ "$WP_VERSION" = 'nightly' ] || [ "$WP_VERSION" = 'trunk' ]; then
-    WP_TESTS_TAG="trunk"
+	WP_TESTS_TAG="trunk"
 else
 	# http serves a single offer, whereas https serves multiple. we only want one
 	download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
 	grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
 	LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
-	if [ -z "$LATEST_VERSION" ]; then
+	if [ -z $LATEST_VERSION ]; then
 		echo "Latest WordPress version could not be found"
 		exit 1
 	fi
@@ -60,22 +60,23 @@ install_wp() {
 
 	mkdir -p $WP_CORE_DIR
 
-	if [ $WP_VERSION = 'nightly' ] || [ $WP_VERSION = 'trunk' ]; then
+	if [ "$WP_VERSION" = 'nightly' ] || [ "$WP_VERSION" = 'trunk' ]; then
 		mkdir -p $TMPDIR/wordpress-nightly
 		download https://wordpress.org/nightly-builds/wordpress-latest.zip  $TMPDIR/wordpress-nightly/wordpress-nightly.zip
 		unzip -q $TMPDIR/wordpress-nightly/wordpress-nightly.zip -d $TMPDIR/wordpress-nightly/
 		mv $TMPDIR/wordpress-nightly/wordpress/* $WP_CORE_DIR
 	else
-		if [ $WP_VERSION = 'latest' ]; then
+		if [ "$WP_VERSION" = 'latest' ]; then
 			local ARCHIVE_NAME='latest'
-		elif echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+'; then
-			if echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0]'; then
+		elif [ $(echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+') ]; then
+			if [ $(echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0]') ]; then
 				# version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
 				LATEST_VERSION=${WP_VERSION%??}
 			else
-			# https serves multiple offers, whereas http serves single.
-			download https://api.wordpress.org/core/version-check/1.7/ $TMPDIR/wp-latest.json
 				# otherwise, scan the releases and get the most up to date minor version of the major release
+				# https serves multiple offers, whereas http serves single.
+				download https://api.wordpress.org/core/version-check/1.7/ $TMPDIR/wp-latest.json
+
 				local VERSION_ESCAPED=`echo $WP_VERSION | sed 's/\./\\\\./g'`
 				LATEST_VERSION=$(grep -o '"version":"'$VERSION_ESCAPED'[^"]*' $TMPDIR/wp-latest.json | sed 's/"version":"//' | head -1)
 			fi
@@ -125,7 +126,7 @@ install_test_suite() {
 }
 
 recreate_db() {
-	if echo "$1" | grep -Eqi '^(y|yes)$'; then
+	if [ $(echo $1 | grep -Eqi '^(y|yes)$') ]; then
 		mysqladmin drop $DB_NAME -f --user="$DB_USER" --password="$DB_PASS"$EXTRA
 		create_db
 		echo "Recreated the database ($DB_NAME)."
@@ -140,12 +141,12 @@ create_db() {
 
 install_db() {
 
-	if [ ${SKIP_DB_CREATE} = "true" ]; then
+	if [ "$SKIP_DB_CREATE" = "true" ]; then
 		return 0
 	fi
 
 	# parse DB_HOST for port or socket references
-    local PARTS=$(echo "$DB_HOST" | grep -Eq '^.*\:.*$')
+	local PARTS=$(echo "$DB_HOST" | grep -Eq '^.*\:.*$')
 	local DB_HOSTNAME=$(echo "$PARTS" | sed -E 's/^(.*)\:(.*)$/\1/')
 	local DB_SOCK_OR_PORT=$(echo "$PARTS" | sed -E 's/^(.*)\:(.*)$/\2/')
 	local EXTRA=""
