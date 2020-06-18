36f23d864f1b86d56b961738fd5b5d6de00081a9
navigatrum <ab@Lenovo-PC.localdomain>
2020-06-18 17:56:23 +0200

    recreate_db update 2

--- templates/install-wp-tests.sh
+++ templates/install-wp-tests.sh
@@ -126,7 +126,6 @@ install_test_suite() {
 
 recreate_db() {
 	if echo "$1" | grep -Eqi '^(y|yes)$'; then
-	then
 		mysqladmin drop $DB_NAME -f --user="$DB_USER" --password="$DB_PASS"$EXTRA
 		create_db
 		echo "Recreated the database ($DB_NAME)."
