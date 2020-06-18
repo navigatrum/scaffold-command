bb699ae88eb209c6dea2bb67ea3f567b00f679fd
navigatrum <ab@Lenovo-PC.localdomain>
2020-06-18 16:52:20 +0200

    first block to sh

--- templates/install-wp-tests.sh
+++ templates/install-wp-tests.sh
@@ -25,27 +25,26 @@ download() {
     fi
 }
 
-if [[ $WP_VERSION =~ ^[0-9]+\.[0-9]+\-(beta|RC)[0-9]+$ ]]; then
-	WP_BRANCH=${WP_VERSION%\-*}
-	WP_TESTS_TAG="branches/$WP_BRANCH"
-
-elif [[ $WP_VERSION =~ ^[0-9]+\.[0-9]+$ ]]; then
-	WP_TESTS_TAG="branches/$WP_VERSION"
-elif [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
-	if [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0] ]]; then
-		# version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
-		WP_TESTS_TAG="tags/${WP_VERSION%??}"
-	else
-		WP_TESTS_TAG="tags/$WP_VERSION"
-	fi
-elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-	WP_TESTS_TAG="trunk"
+if echo "$WP_VERSION" | grep -Eq '^[0-9]+\.[0-9]+\-(beta|RC)[0-9]+$'; then
+    WP_BRANCH=${WP_VERSION%\-*}
+    WP_TESTS_TAG="branches/$WP_BRANCH"
+elif echo "$WP_VERSION" | grep -Eq '^[0-9]+\.[0-9]+$'; then
+    WP_TESTS_TAG="branches/$WP_VERSION"
+elif echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0-9]+'; then
+    if echo "$WP_VERSION" | grep -Eq '[0-9]+\.[0-9]+\.[0]'; then
+        # version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
+        WP_TESTS_TAG="tags/${WP_VERSION%??}"
+    else
+        WP_TESTS_TAG="tags/$WP_VERSION"
+    fi
+elif [ "$WP_VERSION" = 'nightly' ] || [ "$WP_VERSION" = 'trunk' ]; then
+    WP_TESTS_TAG="trunk"
 else
 	# http serves a single offer, whereas https serves multiple. we only want one
 	download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
 	grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
 	LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
-	if [[ -z "$LATEST_VERSION" ]]; then
+	if [ -z "$LATEST_VERSION" ]; then
 		echo "Latest WordPress version could not be found"
 		exit 1
 	fi
